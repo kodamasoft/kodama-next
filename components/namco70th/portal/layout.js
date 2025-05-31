61396d302a0edb1bd0064a9ac4db2a994560d843
banbeucmas <banbeucams@gmail.com>
2025-06-01 01:26:59 +0900

    Properly 70 :')

--- components/namco70th/portal/layout.js
+++ components/namco70th/portal/layout.js
@@ -1,7 +1,7 @@
 export default function Layout({ children }) {
 	return (
 		<div>
-			<div className="min-h-screen bg-namco50-p-background">
+			<div className="min-h-screen bg-namco70-p-background">
 				<main className="h-dvh">{children}</main>
 			</div>
 		</div>
--- pages/namco70th.page.js
+++ pages/namco70th.page.js
@@ -1,14 +1,14 @@
 import Head from 'next/head';
-import Layout from '../components/namco50th/portal/layout';
+import Layout from '../components/namco70th/portal/layout';
 import Image from 'next/image';
-import Logo from '../public/assets/namco50th/portal/images/lg-white.png';
+import Logo from '../public/assets/namco70th/portal/images/lg-white.png';
 
 export default function Discography() {
 	return (
 		<>
 			<Layout>
 				<Head>
-					<title>Namco 50th Anniversary</title>
+					<title>Namco 70th Anniversary</title>
 				</Head>
 				<div className="w-full h-full m-auto">
 					<div className="flex flex-col items-center justify-center h-full w-full gap-8 lg:gap-12">
@@ -19,7 +19,7 @@ export default function Discography() {
 							className="w-64 lg:w-96"
 						/>
 
-						<p className="font-namco50-mono text-2xl animate-pulse lg:text-4xl">
+						<p className="font-namco70-mono text-2xl animate-pulse lg:text-4xl">
 							Coming soon
 						</p>
 					</div>
