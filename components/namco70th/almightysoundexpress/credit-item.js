bd40a03a3f992b5f2c64eadcac61d0e8e4556737
Phạm Đình Duy <banbeucams@gmail.com>
2025-06-06 11:48:56 +0900

    Adding credit page

--- components/namco70th/almightysoundexpress/credit-item.js
+++ components/namco70th/almightysoundexpress/credit-item.js
@@ -7,26 +7,29 @@ export default function CreditItem({
 	socials = [],
 }) {
 	return (
-		<div className="flex justify-between">
-			<div className="flex gap-2 items-center">
+		<div className="flex justify-between py-4 border-b border-namco70-p-stroke/60">
+			<div className="flex gap-4 items-center">
 				<span>{name}</span>
-				{socials.map((social) => (
-					<Tooltip key={social.href}>
-						<TooltipTrigger asChild>
-							<Link
-								className="flex gap-2 opacity-60 hover:opacity-100 transition-opacity duration-200"
-								key={social.href}
-								href={social.href}
-								aria-label={social.title}
-							>
-								{social.icon}
-							</Link>
-						</TooltipTrigger>
-						<TooltipContent>
-							<span>{social.title}</span>
-						</TooltipContent>
-					</Tooltip>
-				))}
+				<div className="flex gap-2 items-center">
+					{socials.map((social) => (
+						<Tooltip key={social.href}>
+							<TooltipTrigger asChild>
+								<Link
+									className="flex gap-2 opacity-60 hover:opacity-100 transition-opacity duration-200"
+									target="_blank"
+									rel="noopener noreferrer"
+									href={social.href}
+									aria-label={social.title}
+								>
+									{social.icon}
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent>
+								<span>{social.title}</span>
+							</TooltipContent>
+						</Tooltip>
+					))}
+				</div>
 			</div>
 			<div>
 				<span>{role}</span>
--- components/namco70th/almightysoundexpress/credit.js
+++ components/namco70th/almightysoundexpress/credit.js
@@ -1,7 +1,126 @@
-import { SiX } from '@icons-pack/react-simple-icons';
+import {
+	SiBandcamp,
+	SiBluesky,
+	SiSoundcloud,
+	SiX,
+	SiYoutube,
+} from '@icons-pack/react-simple-icons';
 import CreditItem from './credit-item';
 
 const credits = [
+	{
+		name: 'ehm',
+		role: 'Artist',
+		socials: [
+			{
+				href: 'https://x.com/EhMehShmeh',
+				title: 'X',
+				icon: <SiX size={20} />,
+			},
+			{
+				href: 'soundcloud.com/ehmehshmeh',
+				title: 'SoundCloud',
+				icon: <SiSoundcloud size={20} />,
+			},
+		],
+	},
+	{
+		name: 'Rapid Riddim',
+		role: 'Artist',
+		socials: [
+			{
+				href: 'https://botnit.bandcamp.com/',
+				title: 'Bandcamp',
+				icon: <SiBandcamp size={20} />,
+			},
+		],
+	},
+	{
+		name: 'Aquatic Enigmas',
+		role: 'Artist',
+		socials: [
+			{
+				href: 'https://botnit.bandcamp.com/',
+				title: 'Bandcamp',
+				icon: <SiBandcamp size={20} />,
+			},
+		],
+	},
+	{
+		name: 'Neon Serenade',
+		role: 'Artist',
+		socials: [
+			{
+				href: 'https://botnit.bandcamp.com/',
+				title: 'Bandcamp',
+				icon: <SiBandcamp size={20} />,
+			},
+		],
+	},
+	{
+		name: 'fusoxide',
+		role: 'Artist',
+		socials: [
+			{
+				href: 'https://x.com/fusoxide',
+				title: 'X',
+				icon: <SiX size={20} />,
+			},
+			{
+				href: 'https://bsky.app/profile/fusoxide.bsky.social',
+				title: 'Bluesky',
+				icon: <SiBluesky size={20} />,
+			},
+		],
+	},
+	{
+		name: 'viravax',
+		role: 'Artist',
+		socials: [
+			{
+				href: 'https://viravax.bandcamp.com/',
+				title: 'Bandcamp',
+				icon: <SiBandcamp size={20} />,
+			},
+			{
+				href: 'https://soundcloud.com/viravax',
+				title: 'SoundCloud',
+				icon: <SiSoundcloud size={20} />,
+			},
+			{
+				href: 'https://www.youtube.com/channel/UCz7xLF93bFM7OeczJg5uDcQ',
+				title: 'YouTube',
+				icon: <SiYoutube size={20} />,
+			},
+		],
+	},
+	{
+		name: 'marklincadet',
+		role: 'Artist',
+		socials: [
+			{
+				href: 'https://bsky.app/profile/marklincadet.bsky.social',
+				title: 'Bluesky',
+				icon: <SiBluesky size={20} />,
+			},
+		],
+	},
+	{
+		name: '_pulstar',
+		role: 'Design',
+		socials: [
+			{
+				href: 'https://x.com/_pulstar',
+				title: 'X',
+				icon: <SiX size={20} />,
+			},
+			{
+				href: 'https://bsky.app/profile/pulstar.bsky.social',
+				title: 'Bluesky',
+				icon: <SiBluesky size={20} />,
+			},
+		],
+	},
 	{
 		name: 'Banbeucmas',
 		role: 'Web Design',
@@ -9,7 +128,7 @@ const credits = [
 			{
 				href: 'https://x.com/Banbeucams',
 				title: 'X',
-				icon: <SiX size={16} />,
+				icon: <SiX size={20} />,
 			},
 		],
 	},
@@ -20,7 +139,7 @@ export default function Credit() {
 			<div>
 				<h2 className="font-medium text-2xl">Credit</h2>
 			</div>
-			<div className="flex flex-col gap-4">
+			<div className="flex flex-col">
 				{credits.map((credit) => (
 					<CreditItem key={credit.name} {...credit} />
 				))}
