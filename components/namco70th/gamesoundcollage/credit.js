7c39c2d209968909d072267ca611b2f7f8b00705
Phạm Đình Duy <banbeucams@gmail.com>
2025-06-23 11:37:31 +0900

    Credit page for gsc

--- components/namco70th/gamesoundcollage/credit.js
+++ components/namco70th/gamesoundcollage/credit.js
@@ -1,81 +1,164 @@
 import {
 	SiBandcamp,
 	SiBluesky,
+	SiCarrd,
 	SiSoundcloud,
 	SiX,
 	SiYoutube,
 } from '@icons-pack/react-simple-icons';
 import CreditItem from './credit-item';
+import { Globe } from 'lucide-react';
 
 const credits = [
 	{
 		name: 'ZahranW',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'youtube.com/@ZahranW',
+				title: 'YouTube',
+				icon: <SiYoutube size={20} />,
+			},
+		],
 	},
 	{
 		name: 'Kaseboy Advance',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'kaseboy-advance.bandcamp.com',
+				title: 'Bandcamp',
+				icon: <SiBandcamp size={20} />,
+			},
+			{
+				href: 'soundcloud.com/kaseboy_advance',
+				title: 'SoundCloud',
+				icon: <SiSoundcloud size={20} />,
+			},
+		],
 	},
 	{
 		name: 'crazygoji',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'crazygoji.bandcamp.com',
+				title: 'Bandcamp',
+				icon: <SiBandcamp size={20} />,
+			},
+		],
 	},
 	{
 		name: 'kpwu',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'kpwu.observer',
+				title: 'Personal Website',
+				icon: <Globe size={20} />,
+			},
+		],
 	},
 	{
 		name: 'jaxcheese',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'jaxcheese.bandcamp.com',
+				title: 'Bandcamp',
+				icon: <SiBandcamp size={20} />,
+			},
+			{
+				href: 'jaxcheese.net',
+				title: 'Personal Website',
+				icon: <Globe size={20} />,
+			},
+		],
 	},
 	{
 		name: 'ScarletLotusProductions',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'scarletlotus.carrd.co',
+				title: 'Carrd',
+				icon: <SiCarrd size={20} />,
+			},
+		],
 	},
 	{
 		name: 'MasterofDed',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'ptweb.me/profile/688',
+				title: 'PxTune Web Profile',
+				icon: <Globe size={20} />,
+			},
+		],
 	},
 	{
 		name: 'MyuPicks',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'soundcloud.com/musicalpixels',
+				title: 'SoundCloud',
+				icon: <SiSoundcloud size={20} />,
+			},
+			{
+				href: 'ptweb.me/profile/483',
+				title: 'PxTune Web Profile',
+				icon: <Globe size={20} />,
+			},
+		],
 	},
 	{
 		name: 'Garoslaw',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'soundcloud.com/garoslaw',
+				title: 'SoundCloud',
+				icon: <SiSoundcloud size={20} />,
+			},
+		],
 	},
 	{
 		name: 'ehm',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'soundcloud.com/ehmehshmeh',
+				title: 'SoundCloud',
+				icon: <SiSoundcloud size={20} />,
+			},
+			{
+				href: 'ptweb.me/profile/482',
+				title: 'PxTune Web Profile',
+				icon: <Globe size={20} />,
+			},
+		],
 	},
 	{
 		name: 'Snuroo',
 		role: 'Artist',
-		socials: [],
+		socials: [
+			{
+				href: 'youtube.com/@snuroo_CK',
+				title: 'YouTube',
+				icon: <SiYoutube size={20} />,
+			},
+		],
 	},
 	{
-		name: '_pulstar',
+		name: 'TiffanyHeat',
 		role: 'Design',
 		socials: [
 			{
-				href: 'https://x.com/_pulstar',
-				title: 'X',
-				icon: <SiX size={20} />,
-			},
-			{
-				href: 'https://bsky.app/profile/pulstar.bsky.social',
-				title: 'Bluesky',
-				icon: <SiBluesky size={20} />,
+				href: 'https://spacehey.com/tiffanyneat',
+				title: 'SpaceHey',
+				icon: <Globe size={20} />,
 			},
 		],
 	},
@@ -87,6 +170,12 @@ const credits = [
 				href: 'https://x.com/Banbeucams',
 				title: 'X',
 				icon: <SiX size={20} />,
+			},
+
+			{
+				href: 'https://banbeu.com',
+				title: 'Personal Website',
+				icon: <Globe size={20} />,
 			},
 		],
 	},
