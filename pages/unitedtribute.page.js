64e73ec5a0a12617b7e31156d62d8bdfe7825f50
default <banbeucams@gmail.com>
2025-09-07 18:16:43 +0000

    Adding speedstar to United Tribute

--- pages/unitedtribute.page.js
+++ pages/unitedtribute.page.js
@@ -11,6 +11,7 @@ import PortalPagination from '../components/namco70th/portal/pagination';
 import PortalEntrance from '../components/namco70th/portal/entrance';
 import KTMLogo from '/public/assets/namco70th/katamari/ktm-logo.webp';
 import SoraLogo from '/public/assets/namco70th/sorasuite/sora-logo.png';
+import SSLogo from '/public/assets/namco70th/speedstars/ss-logo.png';
 
 const projectInfos = [
 	{
@@ -55,11 +56,12 @@ const projectInfos = [
 		isDisabled: false,
 	},
 	{
-		name: '?????????',
-		image: '',
-		presentedBy: 'Team Name',
-		href: '/unitedtribute/almightysoundexpress',
-		isDisabled: true,
+		name: 'SpeedSTARS -a Ridge Racer tribute album-',
+		image: SSLogo,
+		presentedBy: 'COUNTERFEST RECORDS',
+		releasedAt: '2025.09.01',
+		href: 'https://counterfest.kodamasoft.net/releases/speedstars',
+		isDisabled: false,
 	},
 	{
 		name: '?????????',
