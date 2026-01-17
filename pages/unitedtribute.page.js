61cc405abcc41ff23397c53b50afae9b10604bc5
banbeucmas <banbeucams@gmail.com>
2026-01-17 22:39:27 +0900

    Added Starlit Reviere to United Tribute

--- pages/unitedtribute.page.js
+++ pages/unitedtribute.page.js
@@ -11,6 +11,7 @@ import PortalPagination from '../components/namco70th/portal/pagination';
 import PortalEntrance from '../components/namco70th/portal/entrance';
 import KTMLogo from '/public/assets/namco70th/katamari/ktm-logo.webp';
 import SoraLogo from '/public/assets/namco70th/sorasuite/sora-logo.png';
+import StarlitLogo from '/public/assets/namco70th/starlit/starlit-logo.png';
 
 const projectInfos = [
 	{
@@ -55,11 +56,12 @@ const projectInfos = [
 		isDisabled: false,
 	},
 	{
-		name: '?????????',
-		image: '',
-		presentedBy: 'Team Name',
-		href: '/unitedtribute/almightysoundexpress',
-		isDisabled: true,
+		name: 'Starlit Reverie',
+		image: StarlitLogo,
+		releasedAt: '2025.12.29',
+		presentedBy: 'Resonant Union',
+		href: 'https://www.youtube.com/watch?v=dZCXZRGUxYA',
+		isDisabled: false,
 	},
 	{
 		name: '?????????',
