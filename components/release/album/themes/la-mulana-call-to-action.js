eea4d209407ebbb461511640afe1a2de15fcea99
krz <maxlav78@gmail.com>
2026-06-21 23:04:10 +0200

    Add new collaborator and update La Mulana call-to-action styling

--- components/release/album/themes/la-mulana-call-to-action.js
+++ components/release/album/themes/la-mulana-call-to-action.js
@@ -1,7 +1,7 @@
 export default function LaMulanaCallToAction() {
 	return (
 		<section className="bg-current/5 mt-16 py-8">
-			<h2 className="text-2xl text-center uppercase mb-6 font-black">
+			<h2 className="text-2xl text-center uppercase font-black">
 				AVAILABLE JUNE 27TH
 			</h2>
 		</section>
